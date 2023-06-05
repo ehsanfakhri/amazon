88eba8910fc0fb34202336a1fdd11bbaf4a78e66
Ehsan Fakhri <54832918+ehsanfakhri@users.noreply.github.com>
2023-06-05 14:57:18 +0330

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,3 @@
-"use client";
-import Image from "next/image";
 import SearchIcon from "./icons/SearchIcon";
 import ShopingCartIcon from "./icons/ShopingCart";
 import MenuIcon from "./icons/MenuIcon";
